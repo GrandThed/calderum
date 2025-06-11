86d170188e828df7e1c594ab0c407d7b0043162f
benjac-longevo <benjamin.canas@longevo.com>
2025-06-11 13:57:11 -0300

    feat: enhance CalderumTextField and LoginForm with text input actions and submission handling

--- lib/features/account/widgets/login_form.dart
+++ lib/features/account/widgets/login_form.dart
@@ -5,54 +5,75 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
-class LoginForm extends ConsumerWidget {
+class LoginForm extends ConsumerStatefulWidget {
   const LoginForm({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<LoginForm> createState() => _LoginFormState();
+}
+
+class _LoginFormState extends ConsumerState<LoginForm> {
+  final _formKey = GlobalKey<FormState>();
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    super.dispose();
+    emailController.dispose();
+    passwordController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     final authState = ref.watch(authViewModelProvider);
     final authNotifier = ref.read(authViewModelProvider.notifier);
 
-    final emailController = TextEditingController();
-    final passwordController = TextEditingController();
+    _submitForm() async {
+      if (_formKey.currentState?.validate() ?? false) {
+        await authNotifier.login(emailController.text, passwordController.text);
 
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        CalderumTextField(
-          controller: emailController,
-          hint: 'Email',
-          inputFormatters: [],
-          keyboardType: TextInputType.emailAddress,
-        ),
-        const SizedBox(height: 12),
-        CalderumTextField(
-          controller: passwordController,
-          hint: 'Password',
-          obscureText: true,
-        ),
-        const SizedBox(height: 24),
-        SizedBox(
-          width: double.infinity,
-          child: CalderumButton(
-            filled: true,
-            label: 'Login',
-            isLoading: authState.isLoading,
-            onPressed: () async {
-              await authNotifier.login(
-                emailController.text,
-                passwordController.text,
-              );
+        if (authState.hasError && context.mounted) {
+          ScaffoldMessenger.of(
+            context,
+          ).showSnackBar(SnackBar(content: Text(authState.error.toString())));
+        }
+      }
+    }
 
-              if (authState.hasError && context.mounted) {
-                ScaffoldMessenger.of(context).showSnackBar(
-                  SnackBar(content: Text(authState.error.toString())),
-                );
-              }
-            },
+    return Form(
+      autovalidateMode: AutovalidateMode.onUserInteraction,
+      key: _formKey,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          CalderumTextField(
+            textInputAction: TextInputAction.next,
+            controller: emailController,
+            hint: 'Email',
+            inputFormatters: [],
+            keyboardType: TextInputType.emailAddress,
           ),
-        ),
-      ],
+          const SizedBox(height: 12),
+          CalderumTextField(
+            controller: passwordController,
+            hint: 'Password',
+            obscureText: true,
+            textInputAction: TextInputAction.done,
+            onFieldSubmitted: (_) => _submitForm(),
+          ),
+          const SizedBox(height: 24),
+          SizedBox(
+            width: double.infinity,
+            child: CalderumButton(
+              filled: true,
+              label: 'Login',
+              isLoading: authState.isLoading,
+              onPressed: _submitForm,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
